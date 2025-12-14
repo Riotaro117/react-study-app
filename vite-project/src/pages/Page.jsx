1ab8015a9c45c8ff334935e18e3764c03311dc16
riotaro <rugby117117@gmail.com>
2025-12-15 07:48:57 +0900

    アプリの作成

--- vite-project/src/pages/Page.jsx
+++ vite-project/src/pages/Page.jsx
@@ -1,18 +1,27 @@
+import { useState } from 'react';
+
 const Page = () => {
+  const list = [{ id: '', content: '', time: '' }];
+  const [content, setContent] = useState(list);
+  console.log(list)
+  const inputContent = (e) => setContent(e.target.value)
+  console.log(inputContent)
+
+  // const [time, setTime] = useState();
   return (
     <>
       <h1>学習記録アプリ</h1>
       <h2>目指せ1000時間！</h2>
       <label>
         学習内容
-        <input type="text" />
+        <input type="text" value={content} onChange={inputContent} />
       </label>
       <label>
         学習時間
         <input type="number" />
         時間
       </label>
-      <span>入力されている学習内容: </span>
+      <span>入力されている学習内容:{content} </span>
       <span>入力されている時間: </span>
       <span></span>
       <button>登録</button>
