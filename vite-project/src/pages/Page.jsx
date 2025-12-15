8da1017e00821486c4af34e91dbea98d022df25b
riotaro <rugby117117@gmail.com>
2025-12-15 22:10:49 +0900

    登録ボタンを押すと、学習記録が増える

--- vite-project/src/pages/Page.jsx
+++ vite-project/src/pages/Page.jsx
@@ -1,32 +1,51 @@
 import { useState } from 'react';
 
 const Page = () => {
-  const list = [{ id: '', content: '', time: '' }];
-  const [content, setContent] = useState(list);
-  console.log(list)
-  const inputContent = (e) => setContent(e.target.value)
-  console.log(inputContent)
+  const studyList = [];
+  const [inputVal, setInputVal] = useState('');
+  const [inputTime, setInputTime] = useState('');
+  const [study, setStudy] = useState(studyList);
 
-  // const [time, setTime] = useState();
+  const addList = (e) => {
+    e.preventDefault();
+    const newStudy = { id: study.length + 1, content: inputVal, time: inputTime };
+    setStudy((prev) => [...prev, newStudy]);
+    setInputVal('');
+    setInputTime('');
+    console.log(newStudy); //登録されていることを確認
+  };
+
+
   return (
     <>
       <h1>学習記録アプリ</h1>
       <h2>目指せ1000時間！</h2>
-      <label>
-        学習内容
-        <input type="text" value={content} onChange={inputContent} />
-      </label>
-      <label>
-        学習時間
-        <input type="number" />
-        時間
-      </label>
-      <span>入力されている学習内容:{content} </span>
-      <span>入力されている時間: </span>
-      <span></span>
-      <button>登録</button>
-      <span>入力されていない項目があります</span>
-      <span>合計時間: 1 / 1000(h)</span>
+      <div>
+        <label>
+          学習内容
+          <input type="text" value={inputVal} onChange={(e) => setInputVal(e.target.value)} />
+        </label>
+        <label>
+          学習時間
+          <input type="number" value={inputTime} onChange={(e) => setInputTime(e.target.value)} />
+          時間
+        </label>
+      </div>
+      <div>
+        <span>入力されている学習内容:{inputVal} </span>
+        <span>入力されている時間: {inputTime}時間</span>
+      </div>
+      <span>
+        {study.map((study) => (
+          <div key={study.id}>
+            <span>{study.content}</span>
+            <span>{`${study.time}時間`}</span>
+          </div>
+        ))}
+      </span>
+      <button onClick={addList}>登録</button>
+      <span>{inputTime===undefined && "書きましょう"}</span>
+      <span>合計時間: {} / 1000(h)</span>
     </>
   );
 };
