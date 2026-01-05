dfc481052b0d0f79955696f9b3e903a839126e17
riotaro <rugby117117@gmail.com>
2026-01-06 05:38:55 +0900

    tsファイルの設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css';
 import Page from './pages/Page.tsx';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <Page />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Page/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
