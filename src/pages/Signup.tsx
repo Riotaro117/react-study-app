e039e5d2d92b5707b99ee9e708e3c1dbabcbc433
riotaro <rugby117117@gmail.com>
2026-01-06 11:17:47 +0900

    signup機能実装

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import { authRepository } from '@/modules/auth/auth.repository';
+import { useState } from 'react';
 
 const Signup = () => {
-  return (
-    <div>signup</div>
-  )
-}
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-export default Signup
+  // ユーザー登録の処理
+  const signup = async () => {
+    // await authRepository.signup(name, email, password);
+    const user = await authRepository.signup(name, email, password);
+    console.log(user);
+  };
+  return (
+    <div className="min-h-screen bg-gray-100 py-10 px-4 sm:px-6 lg:px-8">
+      <div className="flex flex-col items-center">
+        <h2 className="text-3xl font-extrabold text-gray-900">学習記録アプリ</h2>
+        <div className="mt-8 w-full max-w-md">
+          <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+            <div className="space-y-6">
+              <div>
+                <label className="block text-sm font-medium text-gray-700" htmlFor="username">
+                  ユーザー名
+                </label>
+                <div className="mt-1">
+                  <input
+                    onChange={(e) => setName(e.target.value)}
+                    value={name}
+                    id="username"
+                    name="username"
+                    placeholder="ユーザー名"
+                    required
+                    type="text"
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-slate-500 focus:border-slate-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+              <div>
+                <label className="block text-sm font-medium text-gray-700" htmlFor="email">
+                  メールアドレス
+                </label>
+                <div className="mt-1">
+                  <input
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    id="email"
+                    name="email"
+                    placeholder="メールアドレス"
+                    required
+                    type="email"
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-slate-500 focus:border-slate-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+              <div>
+                <label className="block text-sm font-medium text-gray-700" htmlFor="password">
+                  パスワード
+                </label>
+                <div className="mt-1">
+                  <input
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    id="password"
+                    name="password"
+                    placeholder="パスワード"
+                    required
+                    type="password"
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-slate-500 focus:border-slate-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+              <div>
+                <button
+                  disabled={name === '' || email === '' || password === ''}
+                  onClick={signup}
+                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-slate-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500 disabled:opacity-50 disabled:cursor-not-allowed"
+                >
+                  登録
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
