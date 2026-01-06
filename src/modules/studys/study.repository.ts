9a31e711404ad69c21bbe0277213aa64b05d564f
riotaro <rugby117117@gmail.com>
2026-01-06 21:12:05 +0900

    DBに追加と表示の機能作成

--- src/modules/studys/study.repository.ts
+++ src/modules/studys/study.repository.ts
@@ -1,14 +1,35 @@
 import { supabase } from '@/lib/supabase';
+import { Study } from './study.entity';
 
 export const studyRepository = {
-  async create(title, content) {
-    const params = { title, content };
+  // 学習内容を登録する処理
+  async create(userId: string, params: { content: string; time: number }) {
+    //先にエラーを書いておく
+    if (params.content == '') {
+      throw new Error('学習内容を書いて下さい');
+    }
+    if (params.time < 1) throw new Error('無効な時間です');
+
     const { data, error } = await supabase
       .from('studys')
-      .insert([{ title: params.title, content: params.content }])
+      .insert({
+        user_id: userId,
+        content: params.content,
+        time: params.time,
+      })
       .select()
       .single();
     if (error != null) throw new Error(error.message);
+
     return data;
+  },
+
+  // DBの内容を取得する処理
+  async find(userId: string): Promise<Study[]> {
+    const { data, error } = await supabase.from('studys').select('*').eq('user_id', userId);
+    if (error != null) console.error(error)
+    // if (error != null) throw new Error(error.message);
+
+    return data ?? [];
   },
 };
