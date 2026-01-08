391810f07ac1655993b65855a07585f51f2bbf24
riotaro <rugby117117@gmail.com>
2026-01-09 05:14:44 +0900

    Layoutの修正

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
-import Layout from './Layout';
+import Layout from './layout';
 
 function App() {
   return (
